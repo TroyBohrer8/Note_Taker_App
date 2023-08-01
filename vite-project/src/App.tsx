9adf7983d301a3ea595024357b8c40425b5b2dd3
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2023-08-01 11:10:19 -0400

    created routes

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,11 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { NewNote } from "./NewNote";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>hello world</h1>}></Route>
-    </Routes>
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>}></Route>
+        <Route path="/new" element={<NewNote />}></Route>
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />}></Route>
+      </Routes>
+    </Container>
   );
 }
 
---
+++ vite-project/src/NewNote.tsx
@@ -0,0 +1,5 @@
+export function NewNote() {
+  return (
+    <h1>New Note</h1>
+  )
+}
